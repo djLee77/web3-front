e66d8c03ecdb30c2943ecc737683df881a771398
ehdrjs4502 <ehdrjs4502@naver.com>
2023-09-05 11:00:12 +0900

    커밋

--- src/components/ToggleMenu.jsx
+++ src/components/ToggleMenu.jsx
@@ -3,132 +3,127 @@ import "../css/ToggleMenu.css";
 import DensityMediumIcon from "@mui/icons-material/DensityMedium";
 
 const ToggleMenu = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isSecondOpen, setIsSecondOpen] = useState(false);
-  const [isThirdOpen, setIsThirdOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const [isSecondOpen, setIsSecondOpen] = useState(false);
+    const [isThirdOpen, setIsThirdOpen] = useState(false);
 
-  const [mainList, setMainList] = useState([]);
-  const [secondLists, setSecondLists] = useState({});
-  const [thirdLists, setThirdLists] = useState({});
+    const [mainList, setMainList] = useState([]);
+    const [secondLists, setSecondLists] = useState({});
+    const [thirdLists, setThirdLists] = useState({});
 
-  const [secondList, setSecondList] = useState([]);
-  const [thirdList, setThirdList] = useState([]);
+    const [secondList, setSecondList] = useState([]);
+    const [thirdList, setThirdList] = useState([]);
 
-  useEffect(() => {
-    // 예제에서는 직접 데이터를 사용하지만 실제 환경에서는 API 호출을 사용해야 합니다.
-    const fetchData = async () => {
-      const response = await fetch("http://localhost:4000/api/categories");
-      const data = await response.json();
-      const categories = data.data.categories[0].child;
+    useEffect(() => {
+        // 예제에서는 직접 데이터를 사용하지만 실제 환경에서는 API 호출을 사용해야 합니다.
+        const fetchData = async () => {
+            const response = await fetch("http://localhost:4000/api/categories");
+            const data = await response.json();
+            const categories = data.data.categories[0].child;
 
-      const mainListTemp = [];
-      const secondListsTemp = {};
-      const thirdListsTemp = {};
+            const mainListTemp = [];
+            const secondListsTemp = {};
+            const thirdListsTemp = {};
 
-      categories.forEach((cat) => {
-        mainListTemp.push(cat.name);
+            categories.forEach((cat) => {
+                mainListTemp.push(cat.name);
 
-        if (cat.child && cat.child.length > 0) {
-          secondListsTemp[cat.name] = cat.child.map((child) => child.name);
+                if (cat.child && cat.child.length > 0) {
+                    secondListsTemp[cat.name] = cat.child.map((child) => child.name);
 
-          cat.child.forEach((subCat) => {
-            if (subCat.child && subCat.child.length > 0) {
-              thirdListsTemp[subCat.name] = subCat.child.map(
-                (child) => child.name
-              );
-            }
-          });
-        }
-      });
+                    cat.child.forEach((subCat) => {
+                        if (subCat.child && subCat.child.length > 0) {
+                            thirdListsTemp[subCat.name] = subCat.child.map((child) => child.name);
+                        }
+                    });
+                }
+            });
 
-      setMainList(mainListTemp);
-      setSecondLists(secondListsTemp);
-      setThirdLists(thirdListsTemp);
+            setMainList(mainListTemp);
+            setSecondLists(secondListsTemp);
+            setThirdLists(thirdListsTemp);
+        };
+
+        fetchData();
+    }, []);
+
+    const ToggleSidebar = () => {
+        setIsOpen(!isOpen);
     };
 
-    fetchData();
-  }, []);
+    const ToggleSecondSidebar = (subject) => {
+        setSecondList(secondLists[subject] || []);
+        setIsSecondOpen(true);
+    };
 
-  const ToggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
+    const ToggleThirdSidebar = (subject) => {
+        setThirdList(thirdLists[subject] || []);
+        setIsThirdOpen(true);
+    };
 
-  const ToggleSecondSidebar = (subject) => {
-    setSecondList(secondLists[subject] || []);
-    setIsSecondOpen(true);
-  };
+    return (
+        <div style={{ float: "left" }}>
+            <div className="btn btn-primary" onClick={ToggleSidebar}>
+                <DensityMediumIcon />
+            </div>
+            <div className={`sidebar ${isOpen ? "active" : ""}`}>
+                <div className="sd-header">
+                    <h4 className="mb-0">카테고리</h4>
+                </div>
+                <div className="sd-body">
+                    <ul>
+                        {mainList.map((subject, index) => (
+                            <li key={index} onClick={() => ToggleSecondSidebar(subject)}>
+                                <a className="sd-link">{subject}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            <div className={`sidebar-overlay ${isOpen ? "active" : ""}`} onClick={ToggleSidebar}></div>
 
-  const ToggleThirdSidebar = (subject) => {
-    setThirdList(thirdLists[subject] || []);
-    setIsThirdOpen(true);
-  };
+            <div className={`second-sidebar ${isSecondOpen ? "active" : ""}`}>
+                <div className="sd-header"></div>
+                <div className="sd-body">
+                    <ul>
+                        {secondList.map((subject, index) => (
+                            <li key={index} onClick={() => ToggleThirdSidebar(subject)}>
+                                <a className="sd-link">{subject}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            <div
+                className={`sidebar-overlay ${isSecondOpen ? "active" : ""}`}
+                onClick={() => {
+                    ToggleSidebar();
+                    setIsSecondOpen(false);
+                }}
+            ></div>
 
-  return (
-    <div style={{ float: "left" }}>
-      <div className="btn btn-primary" onClick={ToggleSidebar}>
-        <DensityMediumIcon />
-      </div>
-      <div className={`sidebar ${isOpen ? "active" : ""}`}>
-        <div className="sd-header">
-          <h4 className="mb-0">카테고리</h4>
+            <div className={`third-sidebar ${isThirdOpen ? "active" : ""}`}>
+                <div className="sd-header"></div>
+                <div className="sd-body">
+                    <ul>
+                        {thirdList.map((item, index) => (
+                            <li key={index}>
+                                <a className="sd-link">{item}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            <div
+                className={`sidebar-overlay ${isThirdOpen ? "active" : ""}`}
+                onClick={() => {
+                    ToggleSidebar();
+                    setIsSecondOpen(false);
+                    setIsThirdOpen(false);
+                }}
+            ></div>
         </div>
-        <div className="sd-body">
-          <ul>
-            {mainList.map((subject, index) => (
-              <li key={index} onClick={() => ToggleSecondSidebar(subject)}>
-                <a className="sd-link">{subject}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-      <div
-        className={`sidebar-overlay ${isOpen ? "active" : ""}`}
-        onClick={ToggleSidebar}
-      ></div>
-
-      <div className={`second-sidebar ${isSecondOpen ? "active" : ""}`}>
-        <div className="sd-header"></div>
-        <div className="sd-body">
-          <ul>
-            {secondList.map((subject, index) => (
-              <li key={index} onClick={() => ToggleThirdSidebar(subject)}>
-                <a className="sd-link">{subject}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-      <div
-        className={`sidebar-overlay ${isSecondOpen ? "active" : ""}`}
-        onClick={() => {
-          ToggleSidebar();
-          setIsSecondOpen(false);
-        }}
-      ></div>
-
-      <div className={`third-sidebar ${isThirdOpen ? "active" : ""}`}>
-        <div className="sd-header"></div>
-        <div className="sd-body">
-          <ul>
-            {thirdList.map((item, index) => (
-              <li key={index}>
-                <a className="sd-link">{item}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-      <div
-        className={`sidebar-overlay ${isThirdOpen ? "active" : ""}`}
-        onClick={() => {
-          ToggleSidebar();
-          setIsSecondOpen(false);
-          setIsThirdOpen(false);
-        }}
-      ></div>
-    </div>
-  );
+    );
 };
 
 export default ToggleMenu;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,6 +28,30 @@ const Home = () => {
   };
 
   /* web3 react에서 제공하는 함수와 변수들
+    const [balance, setBalance] = useState(""); // 토큰
+
+    // 사용자가 연결된 네트워크를 id가 아닌 name 또는 symbol로 보여주기 위한 배열
+    const chainIds = {
+        1: { name: "Ethereum mainnet", symbol: "ETH" },
+        3: { name: "Ropsten", symbol: "RopstenETH" },
+        4: { name: "Rinkeby", symbol: "RinkebyETH" },
+        5: { name: "Goerli", symbol: "GoerliETH" },
+        42: { name: "Kovan", symbol: "KovanETH" },
+        11155111: { name: "Sepolia", symbol: "SepoliaETH" },
+        56: { name: "Binance Smart Chain Mainnet", symbol: "BNB" },
+        97: { name: "Binance Smart Chain Testnet", symbol: "tBNB" },
+        43114: { name: "Avalanche C-Chain", symbol: "AVAX" },
+        137: { name: "Polygon Mainnet", symbol: "MATIC" },
+        80001: { name: "Mumbai", symbol: "MATIC" },
+        42161: { name: "Arbitrum One", symbol: "ETH" },
+        10: { name: "Optimism", symbol: "ETH" },
+        250: { name: "Fantom Opera", symbol: "FTM" },
+        8217: { name: "Klaytn Mainnet Cypress", symbol: "KLAY" },
+        1001: { name: "baobob", symbol: "KLAY" },
+        61: { name: "Ethereum Classic Mainnet", symbol: "ETC" },
+    };
+
+    /* web3 react에서 제공하는 함수와 변수들
         connector: 현재 dapp에 연결된 월렛의 connector 값
         library: web3 provider 제공
         chainId: dapp에 연결된 account의 chainId
@@ -36,133 +60,138 @@ const Home = () => {
         activate: dapp 월렛 연결 기능 수행함수
         deactivate: dapp 월렛 해제 수행함수
     */
-  const { chainId, account, library, active, activate, deactivate } =
-    useWeb3React();
-  console.log(injected);
+    const { chainId, account, library, active, activate, deactivate } =
+        useWeb3React();
+    console.log(injected);
 
-  // 연결 버튼 눌렀을 때 대준 내 목소리가 들려??
-  const handdleConnect = () => {
-    // 만약 이미 연결 돼있으면 연결 해제
-    if (active) {
-      deactivate();
-      return;
-    }
-
-    // 메타마스크 계정
-    activate(injected, (error) => {
-      // 크롬 익스텐션 없을 경우 오류 핸들링
-      if ("/No Ethereum provider was found on window.ethereum/".test(error)) {
-        window.open("https://metamask.io/download.html");
-      }
-    });
-  };
-
-  useEffect(() => {
-    // 계정 연결 됐으면
-    if (account) {
-      // 계정에 연결된 네트워크 코인 가져오기
-      library?.getBalance(account).then((result) => {
-        setBalance(result._hex / 10 ** 18); // 16진수로 보기 힘들게 나와서 바꿔주기
-        console.log("result : ", result);
-      });
-    }
-  }, [account]);
-
-  const testProductList = {
-    code: 200,
-    message: "",
-    data: {
-      items: [
-        {
-          itemId: 100111,
-          name: "이쁜 옷1",
-          image1: "image1",
-          price: 100,
-          rate: 3.4,
-          reviewCount: 10,
-          remaining: 9,
-        },
-        {
-          itemId: 100112,
-          name: "이쁜 옷2",
-          image1: "image1",
-          price: 100,
-          rate: 3.4,
-          reviewCount: 10,
-          remaining: 9,
-        },
-        {
-          itemId: 100113,
-          name: "이쁜 옷3",
-          image1: "image1",
-          price: 100,
-          rate: 3.4,
-          reviewCount: 10,
-          remaining: 9,
-        },
-        {
-          itemId: 100114,
-          name: "이쁜 옷3",
-          image1: "image1",
-          price: 100,
-          rate: 3.4,
-          reviewCount: 10,
-          remaining: 9,
-        },
-        {
-          itemId: 200115,
-          name: "이쁜 옷3",
-          image1: "image1",
-          price: 100,
-          rate: 3.4,
-          reviewCount: 10,
-          remaining: 9,
-        },
-        {
-          itemId: 300110,
-          name: "이쁜 옷3",
-          image1: "image1",
-          price: 100,
-          rate: 3.4,
-          reviewCount: 10,
-          remaining: 9,
-        },
-      ],
-    },
-  };
-
-  return (
-    <div>
-      <div>
-        <p>Account: {account}</p>
-        <p>ChainId: {chainIds[chainId]?.name}</p>
-        <p>
-          Balance : {active ? balance + " " + chainIds[chainId]?.symbol : ""}
-        </p>
-      </div>
-      <div>
-        <button type="button" onClick={handdleConnect}>
-          {active ? "disconnect" : "connect"}
-        </button>
-      </div>
-
-      <div className="card-list">
-        {testProductList.data.items.map((product) => (
-          <Card key={product.itemId} product={product} />
-        ))}
-      </div>
-
-      <style jsx>{`
-        .card-list {
-          display: grid;
-          grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-          grid-template-rows: 1fr 1fr 1fr;
-          grid-gap: 20px;
-          width: 960px;
+    // 연결 버튼 눌렀을 때 대준 내 목소리가 들려??
+    const handdleConnect = () => {
+        // 만약 이미 연결 돼있으면 연결 해제
+        if (active) {
+            deactivate();
+            return;
         }
-      `}</style>
-    </div>
-  );
+
+        // 메타마스크 계정
+        activate(injected, (error) => {
+            // 크롬 익스텐션 없을 경우 오류 핸들링
+            if (
+                "/No Ethereum provider was found on window.ethereum/".test(
+                    error
+                )
+            ) {
+                window.open("https://metamask.io/download.html");
+            }
+        });
+    };
+
+    useEffect(() => {
+        // 계정 연결 됐으면
+        if (account) {
+            // 계정에 연결된 네트워크 코인 가져오기
+            library?.getBalance(account).then((result) => {
+                setBalance(result._hex / 10 ** 18); // 16진수로 보기 힘들게 나와서 바꿔주기
+                console.log("result : ", result);
+            });
+            // TODO : 계정 연결 됐으면 서버에 요청 보내서 식별 ID 받아와야함
+        }
+    }, [account]);
+
+    const testProductList = {
+        code: 200,
+        message: "",
+        data: {
+            items: [
+                {
+                    itemId: 100111,
+                    name: "이쁜 옷1",
+                    image1: "image1",
+                    price: 100,
+                    rate: 3.4,
+                    reviewCount: 10,
+                    remaining: 9,
+                },
+                {
+                    itemId: 100112,
+                    name: "이쁜 옷2",
+                    image1: "image1",
+                    price: 100,
+                    rate: 3.4,
+                    reviewCount: 10,
+                    remaining: 9,
+                },
+                {
+                    itemId: 100113,
+                    name: "이쁜 옷3",
+                    image1: "image1",
+                    price: 100,
+                    rate: 3.4,
+                    reviewCount: 10,
+                    remaining: 9,
+                },
+                {
+                    itemId: 100114,
+                    name: "이쁜 옷3",
+                    image1: "image1",
+                    price: 100,
+                    rate: 3.4,
+                    reviewCount: 10,
+                    remaining: 9,
+                },
+                {
+                    itemId: 200115,
+                    name: "이쁜 옷3",
+                    image1: "image1",
+                    price: 100,
+                    rate: 3.4,
+                    reviewCount: 10,
+                    remaining: 9,
+                },
+                {
+                    itemId: 300110,
+                    name: "이쁜 옷3",
+                    image1: "image1",
+                    price: 100,
+                    rate: 3.4,
+                    reviewCount: 10,
+                    remaining: 9,
+                },
+            ],
+        },
+    };
+
+    return (
+        <div>
+            <div>
+                <p>Account: {account}</p>
+                <p>ChainId: {chainIds[chainId]?.name}</p>
+                <p>
+                    Balance :{" "}
+                    {active ? balance + " " + chainIds[chainId]?.symbol : ""}
+                </p>
+            </div>
+            <div>
+                <button type="button" onClick={handdleConnect}>
+                    {active ? "disconnect" : "connect"}
+                </button>
+            </div>
+
+            <div className="card-list">
+                {testProductList.data.items.map((product) => (
+                    <Card key={product.itemId} product={product} />
+                ))}
+            </div>
+
+            <style jsx>{`
+                .card-list {
+                    display: grid;
+                    grid-template-columns: repeat(5, 1fr);
+                    grid-gap: 20px;
+                    width: 960px;
+                }
+            `}</style>
+        </div>
+    );
 };
 
 export default Home;
