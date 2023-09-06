5c5d7d77ed00107e4b63cc07844c34e413faa3fb
ehdrjs4502 <ehdrjs4502@naver.com>
2023-09-06 17:35:12 +0900

    장바구니 페이지 구현 중

--- src/components/cart/CartList.jsx
+++ src/components/cart/CartList.jsx
@@ -1,56 +1,50 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
 import { Button } from "@mui/material";
 
-export default function CartList() {
-    const [cartList, setCartList] = useState([]);
-
-    const getCartList = async () => {
-        try {
-            const res = await axios.get("http://localhost:4000/api/carts");
-            console.log("장바구니 : ", res.data.data.carts);
-            setCartList(res.data.data.carts);
-        } catch (error) {
-            console.log(error);
-        }
+export default function CartList({ cartList }) {
+    // 삭제 버튼 함수
+    const handleDelBtn = (id) => {
+        console.log(id, " 삭제");
     };
-
-    useEffect(() => {
-        getCartList();
-    }, []);
 
     return (
         <div>
-            {cartList.length === 0 && <div>장바구니에 담은 상품이 없습니다.</div>}
-            <TableContainer component={Paper}>
-                <Table size="small">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="center">상품 사진</TableCell>
-                            <TableCell align="center">상품명</TableCell>
-                            <TableCell align="center">수량</TableCell>
-                            <TableCell align="center">가격</TableCell>
-                            <TableCell align="center">삭제</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {cartList.map((item) => (
-                            <TableRow key={item.cartId}>
-                                <TableCell align="center">
-                                    <img src={item.image} />
-                                </TableCell>
-                                <TableCell align="center">{item.name}</TableCell>
-                                <TableCell align="center">{item.count}</TableCell>
-                                <TableCell align="center">{item.price}</TableCell>
-                                <TableCell align="center">
-                                    <Button>x</Button>
-                                </TableCell>
+            {cartList.length === 0 ? (
+                <div>장바구니에 담은 상품이 없습니다.</div>
+            ) : (
+                <TableContainer component={Paper}>
+                    <Table size="small">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center">상품 사진</TableCell>
+                                <TableCell align="center">상품명</TableCell>
+                                <TableCell align="center">수량</TableCell>
+                                <TableCell align="center">가격</TableCell>
+                                <TableCell align="center">삭제</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                        </TableHead>
+                        <TableBody>
+                            {cartList.map((item) => (
+                                <TableRow key={item.cartId}>
+                                    <TableCell align="center">
+                                        <img src={item.image} alt={item.name} />
+                                    </TableCell>
+                                    <TableCell align="left" sx={{ fontSize: "16px" }}>
+                                        {item.name}
+                                    </TableCell>
+                                    <TableCell align="center">{item.count}</TableCell>
+                                    <TableCell align="center" sx={{ fontWeight: "bold" }}>
+                                        {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        <Button onClick={() => handleDelBtn(item.cartId)}>x</Button>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            )}
         </div>
     );
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,59 @@
 import CartList from "../components/cart/CartList";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../css/Cart.css";
 
 export default function Cart() {
+    const [cartList, setCartList] = useState([]); // 장바구니 목록
+    const [total, setTotal] = useState(0); // 총 금액
+
+    const navigate = useNavigate();
+
+    // 장바구니 목록 가져오기
+    const getCartList = async () => {
+        try {
+            const res = await axios.get("http://localhost:4000/api/carts");
+            console.log("장바구니 : ", res.data.data.carts);
+            setCartList(res.data.data.carts);
+            setTotal(res.data.data.total);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getCartList();
+    }, []);
+
+    // 결제 버튼 함수
+    const handlePayBtn = () => {};
+
+    // 취소 버튼 함수
+    const handleBackBtn = () => {
+        navigate("/");
+    };
+
     return (
         <div>
             <h4>장바구니 목록</h4>
-            <CartList />
+            <CartList cartList={cartList} />
+            <div className="box">
+                <div className="total-box">
+                    <span>총 주문 금액 : </span>
+                    <span className="total">{total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</span>
+                </div>
+
+                {/* 버튼 영역 */}
+                <div className="cart-btn-box">
+                    <button className="pay-btn" onClick={handlePayBtn}>
+                        결제하기
+                    </button>
+                    <button className="back-btn" onClick={handleBackBtn}>
+                        계속 쇼핑하기
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
