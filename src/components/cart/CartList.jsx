5ef0aa1b3fb15b53fa55b13e76f54bbbc732a4e5
ehdrjs4502 <ehdrjs4502@naver.com>
2023-09-06 15:48:13 +0900

    장바구니 페이지 구현 중

---
+++ src/components/cart/CartList.jsx
@@ -0,0 +1,3 @@
+export default function CartList() {
+    return <h6>장바구니 목록</h6>;
+}
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,10 @@
+import CartList from "../components/cart/CartList";
+
+export default function Cart() {
+    return (
+        <div>
+            <h4>장바구니</h4>
+            <CartList />
+        </div>
+    );
+}
