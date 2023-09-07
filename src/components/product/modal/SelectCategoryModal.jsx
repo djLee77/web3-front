0006e58eb10ae1edab47ecb09711bdd9e3674bf6
djLee77 <dkdnl232@gmail.com>
2023-09-07 17:22:49 +0900

    all staging

---
+++ src/components/product/modal/SelectCategoryModal.jsx
@@ -0,0 +1,165 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import "../../../css/SelectCategoryModal.css";
+import { useState, useEffect } from "react";
+import { Button } from "@mui/material";
+
+const style = {
+    position: "absolute",
+    top: "40%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 600,
+    height: 450,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+};
+
+export default function SelectCategoryModal({ category, setCategory }) {
+    const [open, setOpen] = useState(false);
+    const [isSecondOpen, setIsSecondOpen] = useState(false);
+    const [isThirdOpen, setIsThirdOpen] = useState(false);
+
+    const [mainList, setMainList] = useState([]);
+    const [secondList, setSecondList] = useState({});
+    const [thirdList, setThirdList] = useState({});
+
+    const [pickedSecondList, setPickedSecondList] = useState([]);
+    const [pickedThirdList, setPickedThirdList] = useState([]);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => {
+        setOpen(false);
+        setIsSecondOpen(false);
+        setIsThirdOpen(false);
+    };
+
+    useEffect(() => {
+        // 예제에서는 직접 데이터를 사용하지만 실제 환경에서는 API 호출을 사용해야 합니다.
+        const fetchData = async () => {
+            const response = await fetch("http://localhost:4000/api/categories");
+            const data = await response.json();
+            const categories = data.data.categories[0].child;
+
+            const secondListsTemp = {};
+            const thirdListsTemp = {};
+
+            // 1번째 카테고리 목록 저장
+            const mainListTemp = categories.map((cat) => ({
+                categoryId: cat.categoryId,
+                name: cat.name,
+            }));
+
+            categories.forEach((cat) => {
+                if (cat.child && cat.child.length > 0) {
+                    // 2번째 카테고리 목록 저장
+                    secondListsTemp[cat.name] = cat.child.map((child) => ({
+                        categoryId: child.categoryId,
+                        name: child.name,
+                    }));
+
+                    cat.child.forEach((subCat) => {
+                        // 3번째 카테고리 목록 저장
+                        if (subCat.child && subCat.child.length > 0) {
+                            thirdListsTemp[subCat.name] = subCat.child.map((child) => ({
+                                categoryId: child.categoryId,
+                                name: child.name,
+                            }));
+                        }
+                    });
+                }
+            });
+
+            setMainList(mainListTemp);
+            setSecondList(secondListsTemp);
+            setThirdList(thirdListsTemp);
+        };
+
+        fetchData();
+    }, []);
+
+    // 선택된 첫번째 카테고리의 맞는 두번째 카테고리 목록 저장 함수
+    const ToggleSecondSidebar = (subject) => {
+        setPickedSecondList(secondList[subject] || []);
+        setIsSecondOpen(true);
+    };
+
+    // 선택된 두번째 카테고리의 맞는 세번째 카테고리 목록 저장 함수
+    const ToggleThirdSidebar = (subject) => {
+        setPickedThirdList(thirdList[subject] || []);
+        setIsThirdOpen(true);
+    };
+
+    return (
+        <div>
+            <button onClick={handleOpen}>카테고리 선택</button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <h4>카테고리</h4>
+                    <div className="category-box">
+                        <div>
+                            <ul className="category-ul">
+                                {mainList.map((subject) => (
+                                    <li
+                                        key={subject.categoryId}
+                                        onClick={() => {
+                                            ToggleSecondSidebar(subject.name);
+                                            setCategory(subject);
+                                        }}
+                                    >
+                                        <a className="sd-link">{subject.name}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div>
+                            <div>
+                                <ul className="category-ul">
+                                    {pickedSecondList.map((subject) => (
+                                        <li
+                                            key={subject.categoryId}
+                                            onClick={() => {
+                                                ToggleThirdSidebar(subject.name);
+                                                setCategory(subject);
+                                            }}
+                                        >
+                                            <a className="sd-link">{subject.name}</a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <ul className="category-ul">
+                                    {pickedThirdList.map((item) => (
+                                        <li key={item.categoryId} onClick={() => setCategory(item)}>
+                                            <a className="sd-link">{item.name}</a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="selected-category-box">
+                        <h5>선택한 카테고리 : {category.name}</h5>
+                    </div>
+                    <div className="btn-box">
+                        <Button variant="contained" onClick={() => setOpen(false)}>
+                            선택 완료
+                        </Button>
+                    </div>
+                </Box>
+            </Modal>
+        </div>
+    );
+}
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,82 @@
+import CartList from "../components/cart/CartList";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import style from "../css/Cart.module.css";
+
+export default function Cart() {
+    const [cartList, setCartList] = useState([]); // 장바구니 목록
+    const [selectAll, setSelectAll] = useState(false); // 상품 전체 선택
+    const [selectedItems, setSelectedItems] = useState([]); // 선택한 상품 목록
+    const [total, setTotal] = useState(0); // 총 금액
+
+    const navigate = useNavigate();
+
+    // 토탈 가격 구하기
+    useEffect(() => {
+        // 선택된 아이템들의 price 값을 합산하여 totalPrice 업데이트
+        let total = 0;
+        selectedItems.forEach((itemId) => {
+            const selectedItem = cartList.find((item) => item.cartId === itemId);
+            if (selectedItem) {
+                total += selectedItem.price;
+            }
+        });
+        setTotal(total);
+    }, [selectedItems, cartList]);
+
+    // 장바구니 목록 가져오는 함수
+    const getCartList = async () => {
+        try {
+            const res = await axios.get("http://localhost:4000/api/carts");
+            console.log("장바구니 : ", res.data.data.carts);
+            setCartList(res.data.data.carts);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    // 처음 마운트 될때 카테고리 가져오기
+    useEffect(() => {
+        getCartList();
+    }, []);
+
+    // 결제 버튼 함수
+    const handlePayBtn = () => {
+        alert("구현 중");
+    };
+
+    // 취소 버튼 함수
+    const handleBackBtn = () => {
+        navigate("/");
+    };
+
+    return (
+        <div>
+            <h4>장바구니 목록</h4>
+            <CartList
+                cartList={cartList}
+                selectAll={selectAll}
+                setSelectAll={setSelectAll}
+                selectedItems={selectedItems}
+                setSelectedItems={setSelectedItems}
+            />
+            <div className={style.box}>
+                <div className={style.totalBox}>
+                    <span>총 주문 금액 : </span>
+                    <span className={style.total}>{total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</span>
+                </div>
+
+                {/* 버튼 영역 */}
+                <div className={style.cartBtnBox}>
+                    <button className={style.payBtn} onClick={handlePayBtn}>
+                        결제하기
+                    </button>
+                    <button className={style.backBtn} onClick={handleBackBtn}>
+                        계속 쇼핑하기
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
