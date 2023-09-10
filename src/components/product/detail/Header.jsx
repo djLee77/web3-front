d80f9f1a1cd9aa5cde777d2839f5dd72641261d5
ehdrjs4502 <ehdrjs4502@naver.com>
2023-09-10 17:12:50 +0900

    상품 상세 페이지 구현

--- src/components/product/detail/Header.jsx
+++ src/components/product/detail/Header.jsx
@@ -1,32 +1,51 @@
 import { TextField } from "@mui/material";
 import style from "../../../css/ProductDetail.module.css";
 import StarRating from "../../StarRating";
+import { useState } from "react";
 
 export default function Detail({ product, reviewRef }) {
-    //
+    const [mainImg, setMainImg] = useState(product.data.image1);
+
+    // 리뷰 보러가는 함수
     const onReviewClick = () => {
         reviewRef.current?.scrollIntoView({ behavior: "smooth" }); // 부드럽게 해당 위치로 이동
+    };
+
+    // 서브 이미지에 마우스 올리면 메인이미지에 이미지 보이도록 하는 함수
+    const onMouseOverImg = (img) => {
+        setMainImg(img);
+    };
+
+    // 장바구니 담기 버튼 클릭 함수
+    const onClickCartBtn = (productId) => {
+        alert(productId + " 장바구니 담기 구현 중");
+    };
+
+    // 구매 버튼 클릭 함수
+    const onClickPayBtn = (productId) => {
+        alert(productId + "구매하기  구현 중");
     };
 
     return (
         <div>
             <div className={style.box}>
-                {/* 이미지 선택 영역 */}
-                <div className={style.imgBox}>
-                    <div className={style.mainImg}>
-                        <img src="" width={300} height={300} alt="이미지1" />
+                <div className={style.subImgBox}>
+                    <div className={style.subImg} onMouseOver={() => onMouseOverImg(product.data.image1)}>
+                        <img src={product.data.image1} alt="이미지1" />
                     </div>
-
-                    <div className={style.imgBox2}>
-                        <div className={style.subImg}>
-                            <img src="" width={145} height={145} alt="이미지2" />
-                        </div>
-                        <div className={style.subImg}>
-                            <img src="" width={145} height={145} alt="이미지3" />
-                        </div>
+                    <div className={style.subImg} onMouseOver={() => onMouseOverImg(product.data.image2)}>
+                        <img src={product.data.image2} alt="이미지2" />
+                    </div>
+                    <div className={style.subImg} onMouseOver={() => onMouseOverImg(product.data.image3)}>
+                        <img src={product.data.image3} alt="이미지3" />
                     </div>
                 </div>
-
+                {/* 이미지 선택 영역 */}
+                <div className={style.mainImgBox}>
+                    <div className={style.mainImg}>
+                        <img src={mainImg} alt="이미지1" />
+                    </div>
+                </div>
                 <div className={style.inpoBox}>
                     <div className={style.rateBox}>
                         <StarRating
@@ -57,8 +76,12 @@ export default function Detail({ product, reviewRef }) {
                             size="small"
                             onChange={(e) => {}}
                         />
-                        <button className={style.payBtn}>바로 구매하기</button>
-                        <button className={style.cartBtn}>장바구니에 담기</button>
+                        <button className={style.payBtn} onClick={() => onClickPayBtn(product.data.itemId)}>
+                            바로 구매하기
+                        </button>
+                        <button className={style.cartBtn} onClick={() => onClickCartBtn(product.data.itemId)}>
+                            장바구니에 담기
+                        </button>
                     </div>
                 </div>
             </div>
