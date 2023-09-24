c531817e07082226e4a978b4cbcf8c35e04c4b51
djLee77 <dkdnl232@gmail.com>
2023-09-24 18:14:48 +0900

    카테고리 결과창 구현 중

--- src/pages/CategoryResult.jsx
+++ src/pages/CategoryResult.jsx
@@ -6,13 +6,49 @@ import Card from "../components/product/Card";
 const CategoryResult = () => {
   const { id } = useParams();
   const [products, setProducts] = useState([]);
+  const [categoryNames, setCategoryNames] = useState([]);
+
+  const findNodeAndPath = (categories, categoryId, path = []) => {
+    for (let category of categories) {
+      if (category.categoryId === categoryId) {
+        return [...path, category.name];
+      }
+      const foundInChild = findNodeAndPath(category.child, categoryId, [
+        ...path,
+        category.name,
+      ]);
+      if (foundInChild) {
+        return foundInChild;
+      }
+    }
+    return null;
+  };
 
   useEffect(() => {
     const getProudctList = async () => {
       try {
+        const categoryRes = await axios.get("/api/public/categories", {
+          headers: {
+            "ngrok-skip-browser-warning": "1234",
+          },
+        });
+        if (categoryRes.data.code === 200) {
+          const pathNames = findNodeAndPath(
+            categoryRes.data.data[0].child,
+            parseInt(id)
+          );
+          setCategoryNames(pathNames || []);
+          console.log(categoryRes.data.data[0].child);
+        }
+
         const res = await axios.get(`/api/public/categories/${id}/items`, {
           headers: {
             "ngrok-skip-browser-warning": "1234",
+          },
+          params: {
+            sortType: "n", // 기본값
+            pageNum: 1, // 기본값
+            pageSize: 6, // 기본값
           },
         });
 
@@ -25,10 +61,11 @@ const CategoryResult = () => {
     };
 
     getProudctList();
-  }, []);
+  }, [id]);
 
   return (
     <div>
+      {categoryNames.join("/")}
       <div className="card-list">
         {products.map((product) => (
           <Card key={product.itemId} product={product} />
