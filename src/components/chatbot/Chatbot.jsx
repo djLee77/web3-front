74829ae25124074ed40f8f3e70bea6424314558f
ehdrjs4502 <ehdrjs4502@naver.com>
2023-10-03 18:31:07 +0900

    챗봇 구현

--- src/components/chatbot/Chatbot.jsx
+++ src/components/chatbot/Chatbot.jsx
@@ -1,21 +1,23 @@
-import { IconButton } from "@mui/material";
+import { Avatar, Button, IconButton } from "@mui/material";
 import style from "../../css/ChatBot.module.css";
 import ClearIcon from "@mui/icons-material/Clear";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import io from "socket.io-client"; // Socket.io 클라이언트 라이브러리를 import합니다.
+import { useRef } from "react";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import SendIcon from "@mui/icons-material/Send";
+
 const socket = io("http://43.201.32.211:3001");
-socket.on("connect", () => {
-    console.log("connected to server");
-});
 
 export default function Chatbot({ setIsOpen, isOpen }) {
     const [inputValue, setInputValue] = useState(""); // 질문 입력칸
-    const [response, setResponse] = useState(""); //  질문에 대한 대답
     const [faqs, setFaqs] = useState([]); // FAQ 목록
-    const [answer, setAnswer] = useState(""); // FAQ에 대한 대답
     const [messages, setMessages] = useState([]); // 채팅 목록 저장
     const [loading, setLoading] = useState(false); // 대답 할 때까지 로딩
+
+    const faqRef = useRef(null);
+    const messageRef = useRef(null);
 
     // FAQ 목록 가져오기
     const fetchQuestions = async () => {
@@ -29,6 +31,10 @@ export default function Chatbot({ setIsOpen, isOpen }) {
 
     // 입력한 메시지 보내기
     const handleSubmit = () => {
+        // 아무것도 안쓰면 보내지 않기
+        if (inputValue.trim() === "") {
+            return;
+        }
         socket.emit("message", inputValue);
         setLoading(true); // 메시지 보내기 전에 로딩 상태를 true로 설정
         setInputValue(""); // 보냈으면 입력창 초기화
@@ -48,8 +54,7 @@ export default function Chatbot({ setIsOpen, isOpen }) {
     };
 
     useEffect(() => {
-        fetchQuestions();
-
+        // 질문에 대한 대답
         socket.on("response", (data) => {
             const message = {
                 message: data.response.text,
@@ -60,6 +65,7 @@ export default function Chatbot({ setIsOpen, isOpen }) {
             setLoading(false);
         });
 
+        // faq에 대한 대답
         socket.on("faq-answer", (data) => {
             const message = {
                 message: data.answer,
@@ -68,12 +74,30 @@ export default function Chatbot({ setIsOpen, isOpen }) {
             setMessages([...messages, message]); // 챗봇이 말한거 메세지에 저장
         });
 
+        // 에러
         socket.on("error", (data) => {
+            const message = {
+                message: data.error,
+                isBot: true,
+            };
+            setMessages([...messages, message]); // 챗봇이 말한거 메세지에 저장
+            setLoading(false);
             console.error(data.error);
-            setResponse("에러가 발생했습니다.");
         });
+
+        messageRef.current?.scrollIntoView({ behavior: "smooth" }); // 부드럽게 해당 위치로 이동
     }, [messages]);
 
+    // 첫 마운트 됐을때
+    useEffect(() => {
+        socket.on("connect", () => {
+            console.log("connected to server");
+        });
+
+        fetchQuestions(); // faq 가져오기
+    }, []);
+
+    // faq 질문 클릭했을때
     const onClickQuestion = (question) => {
         socket.emit("faq-question", question);
         const message = {
@@ -83,30 +107,43 @@ export default function Chatbot({ setIsOpen, isOpen }) {
         setMessages([...messages, message]); // 유저가 선택한 질문 메세지에 저장
     };
 
+    const onClickUpScroll = () => {
+        console.log("클릭");
+        faqRef.current?.scrollIntoView({ behavior: "smooth" }); // 부드럽게 해당 위치로 이동
+    };
+
     return (
         <div className={style.box}>
             <div className={style.header}>
-                <h4>Chat Bot!</h4>
+                <h4>Chat Bot </h4>
                 <IconButton aria-label="delete" onClick={() => setIsOpen(!isOpen)}>
                     <ClearIcon />
                 </IconButton>
             </div>
             <div className={style.contentBox}>
-                <div className={style.faqBox}>
-                    {faqs.map((item) => (
-                        <div className={style.faq} onClick={() => onClickQuestion(item.question)}>
+                <div className={style.faqBox} ref={faqRef}>
+                    {faqs.map((item, idx) => (
+                        <div key={idx} className={style.faq} onClick={() => onClickQuestion(item.question)}>
                             {item.question}
                         </div>
                     ))}
                 </div>
                 <div className={style.messageBox}>
-                    {messages.map((item) => (
-                        <div className={item.isBot ? style.botMessage : style.userMessage}>{item.message}</div>
+                    {messages.map((item, idx) => (
+                        <div key={idx} style={{ display: "flex", marginLeft: "10px" }}>
+                            {item.isBot && <Avatar alt="Remy Sharp" src="/imgs/bot.png" />}
+                            <div className={item.isBot ? style.botMessage : style.userMessage} ref={messageRef}>
+                                {item.message}
+                            </div>
+                        </div>
                     ))}
-                    {loading && <img src="./imgs/Walk.gif" width={80} height={80} />}
+                    {loading && <img src="./imgs/Walk.gif" width={80} height={80} className={style.loadingImg} />}
                 </div>
-                <div className={style.upScroll}>
-                    <span>맨 위로</span>
+
+                <div className={style.upScroll} onClick={onClickUpScroll}>
+                    <IconButton aria-label="delete" size="large">
+                        <KeyboardArrowUpIcon fontSize="inherit" />
+                    </IconButton>
                 </div>
             </div>
             <div className={style.inputBox}>
@@ -117,7 +154,9 @@ export default function Chatbot({ setIsOpen, isOpen }) {
                     onChange={(e) => setInputValue(e.target.value)}
                     onKeyPress={handleOnKeyPress}
                 ></input>
-                <button onClick={handleSubmit}>보내기</button>
+                <Button size="small" onClick={handleSubmit}>
+                    <SendIcon fontSize="small" />
+                </Button>
             </div>
         </div>
     );
