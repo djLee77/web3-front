afb1198319ed79f1b442f980fcd2413008cd8673
ehdrjs4502 <ehdrjs4502@naver.com>
2023-10-21 21:23:20 +0900

    토큰 재발급 서버 주소 변경

--- src/lib/reissueAccToken.js
+++ src/lib/reissueAccToken.js
@@ -2,10 +2,10 @@ import axios from "axios";
 import cookie from "react-cookies";
 
 const reissueAccToken = async () => {
-    const response = await axios.get("/api/users/refresh", {
+    const serverUrl = process.env.REACT_APP_SERVER_URL;
+    const response = await axios.get(`${serverUrl}/api/users/refresh`, {
         headers: {
             Authorization: `Bearer ${cookie.load("refreshToken")}`,
-            "ngrok-skip-browser-warning": "1234",
         },
     });
 
