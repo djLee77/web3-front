2d994ec68b88612885655f310f89b146a3ec1970
ehdrjs4502 <ehdrjs4502@naver.com>
2023-10-21 20:08:02 +0900

    장바구니 css 수정

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -118,9 +118,7 @@ export default function Cart() {
                 <Loading content="장바구니 목록을 불러오는 중입니다.." />
             ) : (
                 <div className={style.box}>
-                    <h4 className={style.title} style={{ marginTop: "5%" }}>
-                        장바구니
-                    </h4>
+                    <h4 className={style.title}>장바구니</h4>
                     <CartList
                         cartList={cartList}
                         selectAll={selectAll}
